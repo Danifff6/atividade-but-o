22158540dd6b7d61ce5257fd207da4ef121d547a
Danifff6 <dl1047454@gmail.com>
2025-03-25 22:22:13 -0300

    botão-incrementador.

--- script.js
+++ script.js
@@ -3,10 +3,17 @@ let contador = 0;
 document.querySelector("#decrementar").addEventListener("click", () => {
     contador--;
     document.getElementById("contador").textContent = contador;
+
+    if (contador === 50) {
+        alert("Inhaí manah, vc tá apertando isso pra que?");
+    }
 });
 
 document.querySelector("#incrementar").addEventListener("click", () => {
     contador++;
     document.getElementById("contador").textContent = contador;
-});
 
+    if (contador === 50) {
+        alert("Inhaí manah, vc tá apertando isso pra que?");
+    }
+});
